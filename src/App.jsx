50549bef439aafc37de1096dda1705d0f503fe17
honzo0481 <gonzalesre@gmail.com>
2017-07-03 16:57:01 -0700

    fixed typo in switch import.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, SWitch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import axios from 'axios'
 
 import UsersList from './components/UsersList'
