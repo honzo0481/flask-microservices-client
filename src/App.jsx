527a390ff41ee0889674d7672cc0aeb35384a784
honzo0481 <gonzalesre@gmail.com>
2017-07-04 00:13:02 -0700

    fixed bug in app.

--- src/App.jsx
+++ src/App.jsx
@@ -48,46 +48,34 @@ class App extends Component {
     obj[event.target.name] = event.target.value
     this.setState(obj)
   }
-  render () {
+  render() {
     return (
       <div>
-        <Navbar
+        <NavBar
           title={this.state.title}
         />
-      </div>
-      <div className="container">
-        <div classname="row">
-          <div className="col-md-6">
-            <br/>
-            <Switch>
-              <Route exact path='/' render={() => (
-                <div>
-                  <h1>All Users</h1>
-                  <hr/><br/>
-                  <AddUser
-                    username={this.state.username}
-                    email={this.state.email}
-                    handleChange={this.handleChange.bind(this)}
-                    AddUser={this.addUser.bind(this)}
-                  />
-                  <br/>
-                  <UsersList users={this.state.users}/>
-                </div>
-              )} />
-              <Route exact path='/about' component={About}/>
-              <Route exact path='/register' render={() => (
-                <Form
-                  formType={'Register'}
-                  formData={this.state.formData}
-                />
-              )} />
-              <Route exact path='/login' render={() => (
-                <Form
-                  formType={'Login'}
-                  formData={this.state.formData}
-                />
-              )} />
-            </Switch>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <br/>
+              <Switch>
+                <Route exact path='/' render={() => (
+                  <div>
+                    <h1>All Users</h1>
+                    <hr/><br/>
+                    <AddUser
+                      username={this.state.username}
+                      email={this.state.email}
+                      handleChange={this.handleChange.bind(this)}
+                      addUser={this.addUser.bind(this)}
+                    />
+                    <br/>
+                    <UsersList users={this.state.users}/>
+                  </div>
+                )} />
+                <Route exact path='/about' component={About}/>
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
