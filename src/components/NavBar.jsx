c54f8e63a99b17a33500229148e506ed0c021bd4
honzo0481 <gonzalesre@gmail.com>
2017-07-04 00:15:18 -0700

    fixed bug in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
-import NavBar from './components/NavBar'
 
 const NavBar = (props) => (
   <Navbar inverse collapseOnSelect>
